272278ea814fb9411f84e0a7ea8f57fc1bae8f51
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-03-23 14:54:44 +0700

    Make ficon being able to read config specify default format

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 #[macro_use]
 extern crate structopt;
-extern crate termion;
+#[macro_use]
+extern crate serde_derive;
 extern crate exitcode;
 extern crate regex;
+extern crate serde;
+extern crate termion;
+extern crate toml;
 
 use ignore::Walk;
-use termion::{color, style};
 use regex::Regex;
+use std::fs;
 use std::path::PathBuf;
 use structopt::StructOpt;
+use termion::{color, style};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "ficon")]
@@ -18,40 +23,80 @@ struct Opt {
     path: PathBuf,
 }
 
+#[derive(Deserialize)]
+struct Config {
+    default: SubConfig,
+}
+
+#[derive(Deserialize)]
+struct SubConfig {
+    format: String,
+}
+
 fn main() {
     let opt: Opt = Opt::from_args();
-    let re_kebab = Regex::new(r"^[a-z][a-z\-]*[a-z]$").unwrap();
+
+    let config_path = if opt.path.is_dir() {
+        format!("{}/{}", opt.path.display(), "ficon.toml")
+    } else {
+        panic!("path specified is not a directory")
+    };
+
+    let config = fs::read_to_string(&config_path)
+        .expect(format!("can't read file from the path specified: {}", config_path).as_str());
+
+    let config: Config = toml::from_str(config.as_str()).unwrap();
+
+    let pattern = match config.default.format.as_str() {
+        "kebab" => Regex::new(r"^[a-z][a-z\-]*[a-z]$").unwrap(),
+        "snake" => Regex::new(r"^[a-z][a-z_]*[a-z]$").unwrap(),
+        "upper_snake" => Regex::new(r"^[A-Z][A-Z_]*$").unwrap(),
+        "camel" => Regex::new(r"^[a-z][A-Za-z]*$").unwrap(),
+        "pascal" => Regex::new(r"^[A-Z][A-Za-z]*$").unwrap(),
+        // TODO:
+        // underscore_pre
+        // underscore_post
+        // underscore_surround
+        _ => panic!("case not found {}", config.default.format),
+    };
+
     let mut ok = true;
 
     for result in Walk::new(opt.path).skip(1) {
         let entry = result.unwrap();
-        let canonical_path = entry.path()
+        let canonical_path = entry
+            .path()
             .canonicalize()
             .expect("can't create canonical path");
 
-        let path_color = if re_kebab.is_match(canonical_path
-            .file_stem()
-            .expect("file stem")
-            .to_str()
-            .expect("file stem to str")
+        if pattern.is_match(
+            canonical_path
+                .file_stem()
+                .expect("file stem")
+                .to_str()
+                .expect("file stem to str"),
         ) {
-            println!("{green}{path}{reset}",
-                     path = format!("{}✓ {}",
-                                    " ".repeat(entry.depth()),
-                                    canonical_path.file_name().unwrap().to_str().unwrap()
-                     ),
-                     green = color::Fg(color::LightGreen),
-                     reset = style::Reset
+            println!(
+                "{green}{path}{reset}",
+                path = format!(
+                    "{}✓ {}",
+                    " ".repeat(entry.depth()),
+                    canonical_path.file_name().unwrap().to_str().unwrap()
+                ),
+                green = color::Fg(color::LightGreen),
+                reset = style::Reset
             );
         } else {
-            println!("{bold}{red}{path}{reset}",
-                     path = format!("{}✘ {}",
-                                    "  ".repeat(entry.depth()),
-                                    canonical_path.file_name().unwrap().to_str().unwrap()
-                     ),
-                     red = color::Fg(color::LightRed),
-                     bold = style::Bold,
-                     reset = style::Reset
+            println!(
+                "{bold}{red}{path}{reset}",
+                path = format!(
+                    "{}✘ {}",
+                    "  ".repeat(entry.depth()),
+                    canonical_path.file_name().unwrap().to_str().unwrap()
+                ),
+                red = color::Fg(color::LightRed),
+                bold = style::Bold,
+                reset = style::Reset
             );
             ok = false;
         };

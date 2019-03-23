36db21b4c52eb8cb28a4694a3eea18d2ce552d12
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-03-23 13:30:17 +0700

    Specift path to check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate structopt;
 extern crate termion;
 extern crate exitcode;
 extern crate regex;
@@ -5,26 +7,38 @@ extern crate regex;
 use ignore::Walk;
 use termion::{color, style};
 use regex::Regex;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "ficon")]
+struct Opt {
+    /// Path to directory to check convention
+    #[structopt(name = "PATH", default_value = ".", parse(from_os_str))]
+    path: PathBuf,
+}
 
 fn main() {
+    let opt: Opt = Opt::from_args();
     let re_kebab = Regex::new(r"^[a-z][a-z\-]*[a-z]$").unwrap();
     let mut ok = true;
 
-    for result in Walk::new(".") {
+    for result in Walk::new(opt.path).skip(1) {
         let entry = result.unwrap();
-        // check if it's kebab, if kebab then green else red
-        let path_color = if re_kebab.is_match(entry
-            .path()
+        let canonical_path = entry.path()
+            .canonicalize()
+            .expect("can't create canonical path");
+
+        let path_color = if re_kebab.is_match(canonical_path
             .file_stem()
-            .unwrap()
+            .expect("file stem")
             .to_str()
-            .unwrap()
+            .expect("file stem to str")
         ) {
             println!("{green}{path}{reset}",
                      path = format!("{}✓ {}",
                                     " ".repeat(entry.depth()),
-                                    entry.path().file_name().unwrap().to_str().unwrap()
+                                    canonical_path.file_name().unwrap().to_str().unwrap()
                      ),
                      green = color::Fg(color::LightGreen),
                      reset = style::Reset
@@ -33,7 +47,7 @@ fn main() {
             println!("{bold}{red}{path}{reset}",
                      path = format!("{}✘ {}",
                                     "  ".repeat(entry.depth()),
-                                    entry.path().file_name().unwrap().to_str().unwrap()
+                                    canonical_path.file_name().unwrap().to_str().unwrap()
                      ),
                      red = color::Fg(color::LightRed),
                      bold = style::Bold,

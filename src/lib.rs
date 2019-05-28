082ecbd0221d17bd0d9b45d61b8c6c3f7cc2dd9d
Sebastian Thiel <sthiel@thoughtworks.com>
2019-05-28 15:41:46 +0530

    Remove borrow-checker dance which turns out to be unnecessary
    It shows that `.as_ref()` is something the checker doesn't really see
    through, leading to issues which should be none.
    Reminder to myself: use 'ref' based destructuring preferably over
    things like 'Option::as_ref()`.

--- src/lib.rs
+++ src/lib.rs
@@ -185,17 +185,12 @@ impl ValidatedConfig {
             .iter_mut()
             .find(|p| p.pattern.matches_path(path))
         {
-            Some(pattern) => {
-                // This is to pacify the borrow checker - I would have hoped '.as_ref()' can be used
-                // and NLL sorts this out. Apparently not if Options are involved.
-                // Problem: We want to optionally update our cache, otherwise return it
-                Ok(match pattern.convention_regex.take() {
-                    Some(regex) => pattern.convention_regex.get_or_insert(regex),
-                    None => pattern
-                        .convention_regex
-                        .get_or_insert(Self::new_regex_for_convention(&pattern.convention)?),
-                })
-            }
+            Some(pattern) => Ok(match pattern.convention_regex {
+                Some(ref regex) => regex,
+                None => pattern
+                    .convention_regex
+                    .get_or_insert(Self::new_regex_for_convention(&pattern.convention)?),
+            }),
             None => Ok(&self.default_convention),
         }
     }
